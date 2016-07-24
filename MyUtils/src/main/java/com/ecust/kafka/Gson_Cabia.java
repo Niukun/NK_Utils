b7811ecc974efee5e0da58a4651774430040d463
Niukun <1464042197@qq.com>
2016-07-24 15:04:51 +0800

    conf read

---
+++ MyUtils/src/main/java/com/ecust/kafka/Gson_Cabia.java
@@ -0,0 +1,20 @@
+package com.ecust.kafka;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+public class Gson_Cabia {
+
+	public static void main(String[] args) throws IOException {
+		JsonParser parser = new JsonParser();
+		
+		JsonObject object = (JsonObject) parser.parse(new FileReader("files/kafka.json"));
+		System.out.println("payload = " + object.get("payload").getAsJsonObject().get("object").getAsString());
+	}
+
+}
