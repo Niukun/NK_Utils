aacc4da5ab9eee68c0e8ab84a51ff81893f05385
Niukun <1464042197@qq.com>
2016-07-26 14:50:27 +0800

    listeners

---
+++ MyUtils/src/main/java/com/ecust/activemq/publish/JMSConsumer2.java
@@ -0,0 +1,39 @@
+package com.ecust.activemq.publish;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+public class JMSConsumer2 {
+	private static final String USERNAME = ActiveMQConnectionFactory.DEFAULT_USER;
+	private static final String PASSWORD = ActiveMQConnectionFactory.DEFAULT_PASSWORD;
+	private static final String BROKERURL = ActiveMQConnectionFactory.DEFAULT_BROKER_URL;
+
+	public static void main(String[] args) {
+		ConnectionFactory connectionFactory;
+		Connection connection = null;
+		Session session;
+		Destination destination;
+		MessageConsumer messageConsumer;
+		connectionFactory = new ActiveMQConnectionFactory(JMSConsumer2.USERNAME, JMSConsumer2.USERNAME,
+				JMSConsumer2.BROKERURL);
+
+		try {
+			connection = connectionFactory.createConnection();
+			connection.start();
+			session = connection.createSession(Boolean.FALSE, Session.AUTO_ACKNOWLEDGE);
+			// destination = session.createQueue("FirstQ");
+			destination = session.createTopic("topic1");
+			messageConsumer = session.createConsumer(destination);
+			messageConsumer.setMessageListener(new Listener());
+		} catch (JMSException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ MyUtils/src/main/java/com/ecust/activemq/publish/Listener.java
@@ -0,0 +1,23 @@
+package com.ecust.activemq.publish;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+/**
+ * 
+ * 消息监听
+ * @author Niukun
+ *
+ */
+public class Listener implements MessageListener{
+
+	public void onMessage(Message message) {
+		try {
+			System.out.println("订阅者收到的消息：" +((TextMessage)message).getText());
+		} catch (JMSException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
