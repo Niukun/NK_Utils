a9858c29f6bc17ebdd4487de6c6d07423bef07c2
Niukun <1464042197@qq.com>
2016-08-08 23:25:01 +0800

    list todo remove

--- MyUtils/src/main/java/com/niukun/list/List.java
+++ MyUtils/src/main/java/com/niukun/list/List.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 public class List {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		ArrayList<String> list = new ArrayList<String>();
 		list.add("123");
 		System.out.println(list.size());
