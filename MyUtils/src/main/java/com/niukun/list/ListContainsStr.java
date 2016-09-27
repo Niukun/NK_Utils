f701e3419c06eaa0d25fd2d9975148f4c2e3941b
Niukun <1464042197@qq.com>
2016-09-27 20:54:44 +0800

    list String
    list里面直接判断是否包含某一数据

---
+++ MyUtils/src/main/java/com/niukun/list/ListContainsStr.java
@@ -0,0 +1,15 @@
+package com.niukun.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListContainsStr {
+
+	public static void main(String[] args) {
+		List list = new ArrayList();
+		for (int i = 0; i < 100; i++) {
+			list.add("abc"+i);
+		}
+		System.out.println(list.contains("abc2"));
+	}
+}
