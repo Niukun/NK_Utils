5a222e9f56a15550195024c3bf3858b31b3cf7c5
Niukun <1464042197@qq.com>
2016-11-06 17:37:25 +0800

    better Random

---
+++ MyUtils/src/main/java/com/niukun/list/RandomList.java
@@ -0,0 +1,29 @@
+package com.niukun.list;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.commons.collections.list.TreeList;
+
+public class RandomList {
+
+	public static void main(String[] args) {
+		Set Randlist = new HashSet();
+		int ii =0;
+		for (int i = 0; i < 67000; i++) {
+			
+			if (Randlist.size() < 5000) {
+				int count = (int) (Math.random() * 67000);
+				Randlist.add(Integer.valueOf(count));
+				ii++;
+			}else{
+				break;
+			}
+		}
+		System.out.println(Randlist.size() + "Randlist.size");
+		System.out.println(ii + "ii");
+	}
+
+}
