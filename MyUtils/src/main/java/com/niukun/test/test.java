2767b738a624908a515469d27a8f0bd9c47dcddc
Niukun <1464042197@qq.com>
2016-11-06 08:36:08 +0800

    fileUtils

--- MyUtils/src/main/java/com/niukun/test/test.java
+++ MyUtils/src/main/java/com/niukun/test/test.java
@@ -1,11 +1,50 @@
 package com.niukun.test;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+
 public class test {
 
-	public static void main(String[] args) {
-		String[] str ={"niukun","niukun2","niukun3"};
-		for(String s :str){
-			System.out.println(s);
+	public static void main(String[] args) throws Exception {
+		long start = System.currentTimeMillis();
+		getNumline(100);
+		System.out.println(System.currentTimeMillis()-start+"mm");
+	}
+	
+	public static void getNumline(int num) throws Exception{
+		InputStreamReader isr = new InputStreamReader(new FileInputStream("D:/NLPIR/test/news_tensite.xml"),"utf-8");
+		
+		BufferedReader bufr = new BufferedReader(isr);
+		String str = null;
+		int i =0;
+		while ((str=bufr.readLine())!=null&&i++<num) {
+			System.out.println(str);
+		}
+		if(bufr!=null){
+			bufr.close();
+		}
+	}
+	
+	public static void FileToUTF8(String code) throws Exception {
+		InputStreamReader isr = new InputStreamReader(new FileInputStream("D:/NLPIR/test/news_tensite_xml.dat"), code);
+		BufferedReader bufr = new BufferedReader(isr);
+		BufferedWriter bufw = new BufferedWriter(new FileWriter("D:/NLPIR/test/news_tensite.xml"));
+		String str = null;
+		int i =0;
+		while ((str=bufr.readLine())!=null) {
+			bufw.write(str);
+			System.out.println(++i);
+		}
+		bufw.flush();
+		if(bufw!=null){
+			bufw.close();
+		}
+		if(bufr!=null){
+			bufr.close();
 		}
 	}
 }
--- MyUtils/src/main/java/file/FileUtils.java
+++ MyUtils/src/main/java/file/FileUtils.java
@@ -9,11 +9,11 @@ import java.io.UnsupportedEncodingException;
 
 public class FileUtils {
 	public static void main(String[] args) throws UnsupportedEncodingException, Exception {
-		FileSplit("D:/NLPIR/test/news_tensite.xml",100);
+		FileSplit(500);
 	}
 
-	private static void FileSplit(String file,int filenumb) throws UnsupportedEncodingException, Exception {
-		InputStreamReader isr = new InputStreamReader(new FileInputStream("D:/NLPIR/test/news_tensite_xml.dat"),"gbk");
+	private static void FileSplit(int filenumb) throws UnsupportedEncodingException, Exception {
+		InputStreamReader isr = new InputStreamReader(new FileInputStream("D:/NLPIR/test/weibo.txt"),"gbk");
 		BufferedReader bufr = new BufferedReader(isr);
 		int num = 0;
 		String str = null;
@@ -22,18 +22,28 @@ public class FileUtils {
 		}
 		System.out.println(num);
 		int singlefilenum = num/filenumb;
-		BufferedReader bufrtem = new BufferedReader(new InputStreamReader(new FileInputStream("D:/NLPIR/test/news_tensite_xml.dat"),"gbk"));
+		BufferedReader bufrtem = new BufferedReader(new InputStreamReader(new FileInputStream("D:/NLPIR/test/weibo.txt"),"gbk"));
 		int count = 0;
 		for (int i = 0; i < filenumb-1; i++) {
-			BufferedWriter bufw = new BufferedWriter(new FileWriter("D:/NLPIR/test/news_tensite_xml/"+i+".txt"));
-			while((str=bufrtem.readLine())!=null&&((count++)<singlefilenum*i)){
+			BufferedWriter bufw = new BufferedWriter(new FileWriter("D:/NLPIR/test/weibo/"+(i+1)+".txt"));
+			while((str=bufrtem.readLine())!=null&&((count++)<singlefilenum*(i+1))){
 				bufw.write(str);
 			}
 			bufw.flush();
 			if(bufw!=null){
 				bufw.close();
 			}
+			System.out.println(i+1+"\tfile finished");
 		}
+		BufferedWriter bufw = new BufferedWriter(new FileWriter("D:/NLPIR/test/weibo/"+filenumb+".txt"));
+		while((str=bufrtem.readLine())!=null){
+			bufw.write(str);
+		}
+		bufw.flush();
+		if(bufw!=null){
+			bufw.close();
+		}
+		System.out.println(filenumb+"\tfile finished");
 	}
 
 }
