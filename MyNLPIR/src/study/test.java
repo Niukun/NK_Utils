e40ba857130f2453dcd7d8d9eb073a5d9883ad58
Niukun <1464042197@qq.com>
2016-12-22 20:48:04 +0800

    new Project NLPIR

---
+++ MyNLPIR/src/study/test.java
@@ -0,0 +1,16 @@
+package study;
+
+import java.util.Properties;
+import java.util.Set;
+
+public class test {
+
+	public static void main(String[] args) {
+//		Properties p = System.getProperties();
+//		Set<Object> set = p.keySet();
+//		for(Object o : set){
+//			System.out.println(o.toString()+":"+System.getProperty(o.toString()));
+//		}
+		System.out.println(System.getProperty("user.dir"));
+	}
+}
