2dfa5023de86baece6e02adde905b5fcdcaf8a00
Niukun <1464042197@qq.com>
2016-12-30 11:02:50 +0800

    第二次得到词向量

--- word2vec/src/main/java/org/acc/word2vec/demo/RestoreDemo.java
+++ word2vec/src/main/java/org/acc/word2vec/demo/RestoreDemo.java
@@ -1,7 +1,11 @@
 package org.acc.word2vec.demo;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import org.acc.word2vec.core.Word2VecUtils;
 import org.deeplearning4j.models.word2vec.Word2Vec;
@@ -19,13 +23,22 @@ public class RestoreDemo {
         	System.out.print(ds[i]+" ");
 		}*/
         System.out.println("______________________");
-        List<String> list= word2Vec.similarWordsInVocabTo("西游记", 0.5);
-        for (int i = 0; i < list.size(); i++) {
-			System.out.println(list.get(i));
-		}
+        List<String> list= new ArrayList();
+        list.add("中国");
+        list.add("北京");
+        list.add("美国");
+        list.add("纽约");
+        Map<String, Double>  map = word2Vec.accuracy(list);
+        HashSet set = (HashSet) map.keySet();
+        Iterator it = set.iterator();
+        while(it.hasNext()){
+        	String str = (String) it.next();
+        	double d = map.get(str);
+        	System.out.println(str + ":" + d);
+        }
+        
+        
         System.out.println(word2Vec.wordsNearest("西游记", 10));
-        System.out.println(word2Vec.wordsNearest("阅读", 10));
-        System.out.println(word2Vec.wordsNearest("读者", 10));
-        System.out.println(word2Vec.wordsNearest("杂技", 10));
+        //[水浒传, 三国演义, 霸王别姬, 红楼梦, 西厢记, 文昭关, 怀玉, 铁梨花, 花木兰, 四郎探母]
     }
 }
