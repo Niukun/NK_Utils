d1607574e3298d4b19162dbf74c98da21bb76114
Niukun <1464042197@qq.com>
2017-02-25 11:29:17 +0800

    添加计划

--- word2vec/src/main/java/data/keyWords.java
+++ word2vec/src/main/java/data/keyWords.java
@@ -7,7 +7,12 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import tfidf.src.org.akgul.TfIdf;
 
 public class keyWords {
 	private static List<String> allDocuments = new ArrayList<String>();
@@ -22,17 +27,45 @@ public class keyWords {
 	public static void main(String[] args) throws Exception {
 		List<String> dataPaths = new ArrayList<String>();
 		dataPaths.add("C:/D/NLPIR/paper/files/test/Normalize/seg/");
-//		dataPaths.add("C:/D/NLPIR/paper/files/train/Normalize/seg/");
-		getIDFSaved(dataPaths);
+		dataPaths.add("C:/D/NLPIR/paper/files/train/Normalize/seg/");
+		getAllDocuments(dataPaths);
+		System.out.println("getAllDocuments finished...");
+		saveDocs();
+		getIDFSave();
+		System.out.println("getIDFSave() finished...");
 	}
-	private static void getIDFSaved(List<String> dataPaths) throws Exception {
+	private static void saveDocs() throws IOException {
+		BufferedWriter bu = new BufferedWriter(new FileWriter(new File("C:/D/NLPIR/paper/files/doc.txt")));
+		Iterator iter = allDocuments.iterator();
+		while(iter.hasNext()){
+			bu.write((String)iter.next());
+			bu.newLine();
+			bu.flush();
+		}
+		
+		
+	}
+	private static void getIDFSave() throws IOException {
+		TfIdf tfIdf = new TfIdf(allDocuments);
+		Map<String, Double> map = tfIdf.idf();
+		Set<String> keyWords = map.keySet();
+		Iterator iter = keyWords.iterator();
+		String key = null;
+		double score = 0;
+		while(iter.hasNext()){
+			key = (String) iter.next();
+			score = map.get(key);
+			bufw.write(key + "\t" + score);
+			bufw.newLine();
+			bufw.flush();
+		}
+	}
+	private static void getAllDocuments(List<String> dataPaths) throws Exception {
 		String line = null;
 		int count =0;
 		for (String dataPath : dataPaths) {
 			File file = new File(dataPath);
-			System.out.println(file.getAbsolutePath());
 			File[] files = file.listFiles();
-			System.out.println(files.length);
 			for (File f : files) {
 				if(!f.isDirectory()){
 					BufferedReader bufr = new BufferedReader(new FileReader(f));
