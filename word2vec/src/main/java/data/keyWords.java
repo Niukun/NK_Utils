c449437672acdf044a738c4b9f4a63fa3b715f7e
Niukun <1464042197@qq.com>
2017-02-25 13:40:34 +0800

    关键字获取
    有参数可调整关键词个数

--- word2vec/src/main/java/data/keyWords.java
+++ word2vec/src/main/java/data/keyWords.java
@@ -9,92 +9,153 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 
+import tfidf.src.org.akgul.MutableInt;
 import tfidf.src.org.akgul.TfIdf;
 
 public class keyWords {
 	private static List<String> allDocuments = new ArrayList<String>();
-	private static BufferedWriter bufw ;
-	static{
+	private static BufferedWriter bufwidf;
+	private static BufferedWriter bufwtf;
+	private static Map<String, Double> idfmap;
+	static {
 		try {
-			bufw = new BufferedWriter(new FileWriter(new File("C:/D/NLPIR/paper/files/idf.txt")));
+			bufwidf = new BufferedWriter(new FileWriter(new File("C:/D/NLPIR/paper/files/idf.txt")));
+			bufwtf = new BufferedWriter(new FileWriter(new File("C:/D/NLPIR/paper/files/tf.txt")));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
+
 	public static void main(String[] args) throws Exception {
 		List<String> dataPaths = new ArrayList<String>();
 		dataPaths.add("C:/D/NLPIR/paper/files/test/Normalize/seg/");
 		dataPaths.add("C:/D/NLPIR/paper/files/train/Normalize/seg/");
+		System.out.println("get allDocuments...");
 		getAllDocuments(dataPaths);
-		System.out.println("getAllDocuments...");
+
+		System.out.println("save allDocuments...");
 		saveDocs();
-		System.out.println("saveDocs...");
-		getIDFSave();
-		System.out.println("getIDFSave()...");
+
+		System.out.println("IDF Save...");
+		getSortedIDFSave();
+
+		System.out.println("TF Save...");
+		getSortedTFIDFSave(15);
+
+		System.out.println("end...");
+
 	}
+
+	private static void getSortedTFIDFSave(int wordsNum) throws IOException {
+		TfIdf tfIdf = new TfIdf(allDocuments);
+
+		for (int i = 0; i < allDocuments.size(); i++) {
+			Map<String, MutableInt> map = tfIdf.tf(allDocuments.get(i));
+			Set<String> keyWords = map.keySet();
+			Iterator iter = keyWords.iterator();
+			Map<String, Double> tfidfmap = new HashMap<String, Double>();
+			// 先得到tfidf的map数据
+			String key = null;
+			double idf = 0;
+			int value = 0;
+			while (iter.hasNext()) {
+				key = (String) iter.next();
+				if (idfmap.containsKey(key)) {
+					idf = idfmap.get(key);
+					value = map.get(key).getCounter();
+//					System.out.println(key + ":" + (value * idf) + "\t");
+					tfidfmap.put(key, (value * idf));
+				}
+			}
+
+			// 按照value大小排序
+			List<Map.Entry<String, Double>> list = new ArrayList<Map.Entry<String, Double>>(tfidfmap.entrySet());
+			Collections.sort(list, new Comparator<Map.Entry<String, Double>>() {
+				// 降序排序
+				public int compare(Entry<String, Double> o1, Entry<String, Double> o2) {
+					return o2.getValue().compareTo(o1.getValue());
+				}
+
+			});
+			int num = 0;
+			for (Map.Entry<String, Double> mapping : list) {
+				if (num++ < wordsNum) {
+					bufwtf.write(mapping.getKey() + ":" + mapping.getValue());
+					bufwtf.newLine();
+					bufwtf.flush();
+				}
+			}
+			bufwtf.write("---------------------------");
+			bufwtf.newLine();
+		}
+	}
+
 	private static void saveDocs() throws IOException {
 		BufferedWriter bu = new BufferedWriter(new FileWriter(new File("C:/D/NLPIR/paper/files/doc.txt")));
 		Iterator iter = allDocuments.iterator();
-		while(iter.hasNext()){
-			bu.write((String)iter.next());
+		while (iter.hasNext()) {
+			bu.write((String) iter.next());
 			bu.newLine();
 			bu.flush();
 		}
 	}
+
 	/**
 	 * 含有map按照value排序：http://www.cnblogs.com/hxsyl/p/3331095.html
+	 * 
 	 * @throws IOException
 	 */
-	private static void getIDFSave() throws IOException {
+	private static void getSortedIDFSave() throws IOException {
 		TfIdf tfIdf = new TfIdf(allDocuments);
-		Map<String, Double> map = tfIdf.idf();
-		Set<String> keyWords = map.keySet();
-		Iterator iter = keyWords.iterator();
-		
-//		按照value大小排序
-		List<Map.Entry<String,Double>> list = new ArrayList<Map.Entry<String,Double>>(map.entrySet());
-		Collections.sort(list,new Comparator<Map.Entry<String,Double>>() {
-            //升序排序
-            public int compare(Entry<String, Double> o1,
-                    Entry<String, Double> o2) {
-                return o1.getValue().compareTo(o2.getValue());
-            }
-            
-        });
-		for(Map.Entry<String,Double> mapping:list){ 
-            bufw.write(mapping.getKey()+":"+mapping.getValue());
-			bufw.newLine();
-			bufw.flush();
-       } 
-		
-//		不按照value大小排序
-//		String key = null;
-//		double score = 0;
-//		while(iter.hasNext()){
-//			key = (String) iter.next();
-//			score = map.get(key);
-//			System.out.println(key + "\t" + score);
-//			bufw.write(key + "\t" + score);
-//			bufw.newLine();
-//			bufw.flush();
-//		}
+		idfmap = tfIdf.idf();
+		// Set<String> keyWords = idfmap.keySet();
+		// Iterator iter = keyWords.iterator();
+
+		// 按照value大小排序
+		List<Map.Entry<String, Double>> list = new ArrayList<Map.Entry<String, Double>>(idfmap.entrySet());
+		Collections.sort(list, new Comparator<Map.Entry<String, Double>>() {
+			// 升序排序
+			public int compare(Entry<String, Double> o1, Entry<String, Double> o2) {
+				return o1.getValue().compareTo(o2.getValue());
+			}
+
+		});
+		for (Map.Entry<String, Double> mapping : list) {
+			bufwidf.write(mapping.getKey() + ":" + mapping.getValue());
+			bufwidf.newLine();
+			bufwidf.flush();
+		}
+
+		// 不按照value大小排序
+		// String key = null;
+		// double score = 0;
+		// while(iter.hasNext()){
+		// key = (String) iter.next();
+		// score = map.get(key);
+		// System.out.println(key + "\t" + score);
+		// bufw.write(key + "\t" + score);
+		// bufw.newLine();
+		// bufw.flush();
+		// }
 	}
+
 	private static void getAllDocuments(List<String> dataPaths) throws Exception {
 		String line = null;
-		int count =0;
+		int count = 0;
 		for (String dataPath : dataPaths) {
 			File file = new File(dataPath);
 			File[] files = file.listFiles();
 			for (File f : files) {
-				if(!f.isDirectory()){
+				if (!f.isDirectory()) {
 					BufferedReader bufr = new BufferedReader(new FileReader(f));
-					while((line=bufr.readLine())!=null){
+					while ((line = bufr.readLine()) != null) {
 						allDocuments.add(line);
 						count++;
 					}
