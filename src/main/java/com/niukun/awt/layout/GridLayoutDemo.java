58720ac79b730f91e3be4ae8348522cdeac15dd4
Niukun <1464042197@qq.com>
2019-01-15 21:55:31 +0800

    GridLayoutDemo

--- src/main/java/com/niukun/awt/layout/GridLayoutDemo.java
+++ src/main/java/com/niukun/awt/layout/GridLayoutDemo.java
@@ -1,7 +1,28 @@
 package com.niukun.awt.layout;
 
-public class GridLayoutDemo {
+import com.niukun.awt.MyFrame;
+
+import java.awt.*;
+
+import static java.awt.BorderLayout.NORTH;
+
+
+public class GridLayoutDemo extends MyFrame {
     public static void main(String[] args){
-        
+        Panel p1 = new Panel();
+        p1.add(new TextField(30));
+        f.add(p1,NORTH);
+        Panel p2 = new Panel();
+        p2.setLayout(new GridLayout(3,5,4,4));
+        String[] name = {"1","2","3","4","5","6","7","8","9","0","+","_","*","/","="};
+        for ( String str : name) {
+            p2.add(new Button(str));
+        }
+        f.add(p2);
+        f.pack();
+        f.setVisible(true);
+
+
+
     }
 }
