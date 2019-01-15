955ee1efd7bffc11d139b08618dd021198b7bd11
Niukun <1464042197@qq.com>
2019-01-16 00:42:39 +0800

    BoxLayoutDemo

---
+++ src/main/java/com/niukun/awt/layout/BoxLayoutDemo.java
@@ -0,0 +1,49 @@
+package com.niukun.awt.layout;
+
+import com.niukun.awt.MyFrame;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * BoxLayout 是Swing提供的布局管理器，用它管理Swing组件会有更好的效果
+ *
+ * glue设置的变量是可变的
+ * strut设置的变量是不可变的
+ */
+public class BoxLayoutDemo extends MyFrame {
+
+    private Box horizontal = Box.createHorizontalBox();
+    private Box vertical = Box.createVerticalBox();
+    public void init(){
+        f.setLayout(new BoxLayout(f,BoxLayout.Y_AXIS));
+        f.add(new Button("First Button"));
+        f.add(new Button("Second Button"));
+        f.pack();
+        f.setVisible(true);
+    }
+
+    public void init2(){
+        horizontal.add(new Button("水平1"));
+        horizontal.add(Box.createHorizontalGlue());
+        horizontal.add(new Button("水平2"));
+        horizontal.add(Box.createHorizontalStrut(10));
+        horizontal.add(new Button("水平3"));
+
+
+        vertical.add(new Button("垂直1"));
+        vertical.add(Box.createVerticalGlue());
+        vertical.add(new Button("垂直2"));
+        vertical.add(Box.createVerticalStrut(10));
+        vertical.add(new Button("垂直3"));
+        f.add(horizontal,BorderLayout.NORTH);
+        f.add(vertical);
+        f.pack();
+        f.setVisible(true);
+    }
+
+
+    public static void main(String[] a){
+        new BoxLayoutDemo().init2();
+    }
+}
