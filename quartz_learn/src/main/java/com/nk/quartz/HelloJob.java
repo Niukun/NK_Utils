034227ffd20ca67452a82b6705c7b5b2c00bd656
牛坤 <niuk@izkml.com>
2020-12-18 19:23:36 +0800

    quartz demo

---
+++ quartz_learn/src/main/java/com/nk/quartz/HelloJob.java
@@ -0,0 +1,54 @@
+package com.nk.quartz;
+
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+public class HelloJob implements Job {
+    @Override
+    public void execute(JobExecutionContext context) throws JobExecutionException {
+
+        Object tv1 = context.getTrigger().getJobDataMap().get("t1");
+        Object tv2 = context.getTrigger().getJobDataMap().get("t2");
+        Object jv1 = context.getJobDetail().getJobDataMap().get("j1");
+        Object jv2 = context.getJobDetail().getJobDataMap().get("j2");
+        Object sv = null;
+        try {
+            sv = context.getScheduler().getContext().get("skey");
+        } catch (SchedulerException e) {
+            e.printStackTrace();
+        }
+        System.out.println(tv1+":"+tv2);
+        System.out.println(jv1+":"+jv2);
+        System.out.println(sv);
+        System.out.println("hello:"+ LocalDateTime.now());
+
+
+    }
+
+    public static void main(String[] args) throws SchedulerException {
+
+        Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+        scheduler.getContext().put("skey", "svalue");
+
+        //创建一个Trigger
+        Trigger trigger = TriggerBuilder.newTrigger()
+                .withIdentity("trigger1", "group1")
+                .usingJobData("t1", "tv1")
+                .withSchedule(SimpleScheduleBuilder.simpleSchedule().withIntervalInSeconds(3)
+                        .repeatForever()).build();
+        trigger.getJobDataMap().put("t2", "tv2");
+
+        //创建一个job
+        JobDetail job = JobBuilder.newJob(HelloJob.class)
+                .usingJobData("j1", "jv1")
+                .withIdentity("myjob", "mygroup").build();
+        job.getJobDataMap().put("j2", "jv2");
+
+        //注册trigger并启动scheduler
+        scheduler.scheduleJob(job,trigger);
+        scheduler.start();
+    }
+}
